eeb1bdf1f7a1396a8d838b68ed27ddcb48155afe
Christophe Heral <christophe.heral@wanteeed.com>
2023-06-25 19:45:39 +0200

    Use zip/unzip & Azure defined port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from './App'
-const port = 3000
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/prefer-nullish-coalescing
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.info(`server is listening on ${port}`)
