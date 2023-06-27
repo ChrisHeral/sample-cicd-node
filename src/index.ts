291a73c89001701d9fc8634f1c0421e4971fddc5
Christophe Heral <christophe.heral@wanteeed.com>
2023-06-27 21:10:33 +0200

    Clean pipeline & code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import app from './App'
-// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/prefer-nullish-coalescing
-const port = process.env.PORT || 3000
+const port = process.env.PORT ?? '3000'
 
 app.listen(port, () => {
   console.info(`server is listening on ${port}`)
